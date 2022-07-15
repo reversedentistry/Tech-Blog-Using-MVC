2e07faca8bf9444582423aa278a3f4af9e52e7af
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-14 23:52:02 -0700

    Created homepage GET routes

---
+++ controllers/homepage-routes.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User, 
+                    attributes: ["name"],
+                }, 
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true})); 
+
+        res.render("homepage", {
+            posts, 
+        })
+
+    } catch (err) {}
+})
