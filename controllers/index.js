f1e8d5ad616a83da837bb1ab2f2239f09ac1318d
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-16 09:41:20 -0700

    Added router functions to fix connection and display

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,8 @@ const apiRoutes = require("./api");
 const homepageRoutes = require("./homepage-routes"); 
 const dashboardRoutes = require("./dashboard-routes"); 
 
+router.use("/", homepageRoutes);
+router.use("/api", apiRoutes); 
+router.use("/dashboard", dashboardRoutes);
+
 module.exports = router; 
