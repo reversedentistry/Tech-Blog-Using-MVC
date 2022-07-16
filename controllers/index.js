9bb969c725d46711f9442e816d18b371e485d256
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-15 23:03:55 -0700

    Created index folder for viewer routes

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router(); 
+
+const apiRoutes = require("./api"); 
+const homepageRoutes = require("./homepage-routes"); 
+const dashboardRoutes = require("./dashboard-routes"); 
+
+module.exports = router; 
