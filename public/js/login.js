aa3883331ab1bbb9156bf36e4a56790ad5325208
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-22 22:28:52 -0700

    Removed email variable and comments

--- public/js/login.js
+++ public/js/login.js
@@ -1,20 +1,17 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
-    // Collect values from the login form
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
     if (username && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users/login', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
         document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
@@ -25,13 +22,13 @@ const loginFormHandler = async (event) => {
   const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#username-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && password) {
+    if (username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
