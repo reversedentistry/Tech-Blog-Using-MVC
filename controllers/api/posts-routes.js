1c9e3de104fb963bd06cd4dcab2f253ad380bdfb
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-23 22:46:32 -0700

    Corrected req + res syntax

--- controllers/api/posts-routes.js
+++ controllers/api/posts-routes.js
@@ -30,19 +30,19 @@ router.get("/:id", async (req, res) => {
     }
 }); 
 
-router.post("/", withAuth, async (res, req) => {
+router.post("/", withAuth, async (req, res) => {
     try {
         const newPost = await Post.create({
             ...req.body,
             user_id: req.session.user_id
         });
-        res.status(200).json(err);
+        res.status(200).json(newPost);
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-router.put("/:id", withAuth, async (res, req) => {
+router.put("/:id", withAuth, async (req, res) => {
     try {
         const updatedPost = await Post.update({
             ...req.body
