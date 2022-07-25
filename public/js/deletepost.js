b918228232e399d2368eb9ea86f7933e5d9555bf
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-24 22:14:27 -0700

    Created post deletion fetch request

---
+++ public/js/deletepost.js
@@ -0,0 +1,21 @@
+const deletePostHandler = async (event) => {
+    event.preventDefault();
+  
+  const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
+  
+      const response = await fetch(`/api/posts/${post_id}`, {
+        method: 'DELETE'        
+      });
+  
+      if (response.ok) {
+        document.location.replace(`/dashboard`)
+      } else {
+        alert('Failed to delete comment');
+      }
+  };
+  
+  
+  
+  document
+    .querySelector('#delete-post').addEventListener('click', deletePostHandler);
+    
