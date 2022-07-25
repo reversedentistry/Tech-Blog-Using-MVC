3bf23dcda05b48c46ee54ff263b02635c95268f6
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-24 22:16:36 -0700

    Corrected json response

--- controllers/api/posts-routes.js
+++ controllers/api/posts-routes.js
@@ -73,7 +73,7 @@ router.delete("/:id", withAuth, async (req, res) => {
         if (!deletedPost) {
             res.status(404).json({ message: "No post with this ID." })
         }
-        res.status(200).json(err)
+        res.status(200).json(deletedPost)
     } catch (err) {
         res.status(500).json(err);
     }
