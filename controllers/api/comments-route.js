a8eae3aa00342b950dc7ccf814e8e6930bd797cb
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-24 21:50:42 -0700

    Changed comment create body

--- controllers/api/comments-route.js
+++ controllers/api/comments-route.js
@@ -24,7 +24,8 @@ router.get("/", async (req, res) => {
 router.post("/", withAuth, async (req, res) => {
     try {
         const newComment = await Comment.create({
-            ...req.body, 
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
             user_id: req.session.user_id
         }); 
         res.status(200).json(newComment);
