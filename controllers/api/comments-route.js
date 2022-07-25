0f98e4ed17daee626b938e3e1098340e7fea59c5
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-24 17:28:51 -0700

    Created POST route for new comments

--- controllers/api/comments-route.js
+++ controllers/api/comments-route.js
@@ -21,4 +21,16 @@ router.get("/", async (req, res) => {
     }
 }); 
 
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body, 
+            user_id: req.session.user_id
+        }); 
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err)
+    }
+}); 
+
 module.exports = router;
