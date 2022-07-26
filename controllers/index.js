982f6da72bb1e3c2e805133a598fb7d067a618fa
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-25 20:31:09 -0700

    removed dashboard routes import

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api"); 
 const homepageRoutes = require("./homepage-routes"); 
-const dashboardRoutes = require("./dashboard-routes"); 
+
 
 router.use("/", homepageRoutes);
 router.use("/api", apiRoutes); 
